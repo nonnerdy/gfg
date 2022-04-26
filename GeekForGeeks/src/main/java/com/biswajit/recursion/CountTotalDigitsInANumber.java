a007097bae5697f668146d6fba2a6f0ff437237b
nonnerdy <biswajiitjana@gmail.com>
2022-04-26 21:18:43 +0530

    fix to support all the test cases.(single digit).

--- GeekForGeeks/src/main/java/com/biswajit/recursion/CountTotalDigitsInANumber.java
+++ GeekForGeeks/src/main/java/com/biswajit/recursion/CountTotalDigitsInANumber.java
@@ -5,8 +5,8 @@ public class CountTotalDigitsInANumber {
         System.out.println(countDigits(987609890));
     }
     public static int countDigits(int n){
-        if(n==0){
-            return 0;
+        if(n<10){
+            return 1;
         }
         else{
             return 1+countDigits(n/10);
