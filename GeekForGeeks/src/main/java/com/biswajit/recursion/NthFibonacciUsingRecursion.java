a990f2464bf7806dea8da7a1dbef736d8592c942
nonnerdy <biswajiitjana@gmail.com>
2022-04-27 20:16:00 +0530

    Nth fibonacci number using recursion.

---
+++ GeekForGeeks/src/main/java/com/biswajit/recursion/NthFibonacciUsingRecursion.java
@@ -0,0 +1,17 @@
+package com.biswajit.recursion;
+
+public class NthFibonacciUsingRecursion {
+    public static void main(String[] args) {
+        System.out.println(fibonacciRecursion(20));
+    }
+
+    public static int fibonacciRecursion(int n){
+            if(n==1){
+                return 1;
+            }
+            if(n==2){
+                return 1;
+            }
+            return fibonacciRecursion(n-1)+fibonacciRecursion(n-2);
+    }
+}
